e895c32471f19240c8fad80bf9e5caf37edc4bea
Victor <Vmalho393@west-mec.edu>
2021-11-10 05:50:08 -0800

    Make + - * / functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+function dis(val) {
+    const display = document.getElementById('result');
+    display.value += "     " + val;
+}
+
+function solve() {
+    const display = document.getElementById('result');
+    let x = display.value;
+    let y = eval(x);
+    display.value = x + "                                                                   " + y
+}
